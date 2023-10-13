c906af3bda2f9ec8ffaf3c5131005dfafb3b962e
Agung Nawawi <agung@fairtech.com.sg>
2023-10-13 22:49:03 +0700

    Create travel model

---
+++ PesonaIndonesia/Model/Pesona.swift
@@ -0,0 +1,38 @@
+//
+//  Pesona.swift
+//  PesonaIndonesia
+//
+//  Created by Agung Nawawi on 13/10/23.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Travel: Hashable, Codable {
+    var id: Int
+    var destination: String
+    var category: String
+    var city: String
+    var province: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinates: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
+}
