924a0959da2f526768a347af9fde584e2d0d5646
Agung Nawawi <agung@fairtech.com.sg>
2023-10-13 21:21:05 +0700

    Initial Commit

---
+++ PesonaIndonesia/PesonaIndonesiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PesonaIndonesiaApp.swift
+//  PesonaIndonesia
+//
+//  Created by Agung Nawawi on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PesonaIndonesiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
