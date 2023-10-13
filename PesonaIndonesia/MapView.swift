9dda3791c838414c233a40facb7d23fdf7a4b324
Agung Nawawi <agung@fairtech.com.sg>
2023-10-13 21:48:18 +0700

    use swiftui view from other framework

---
+++ PesonaIndonesia/MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  PesonaIndonesia
+//
+//  Created by Agung Nawawi on 13/10/23.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(
+                latitude: -7.607382, longitude: 110.203751),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView()
+}
