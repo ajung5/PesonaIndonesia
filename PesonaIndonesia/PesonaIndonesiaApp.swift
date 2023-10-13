6c4997681bc4e34fe34ee7e0a9a6d92c9826f73b
Agung Nawawi <agung@fairtech.com.sg>
2023-10-13 21:23:14 +0700

    create new project

--- PesonaIndonesia/PesonaIndonesiaApp.swift
+++ PesonaIndonesia/PesonaIndonesiaApp.swift
@@ -5,6 +5,10 @@
 //  Created by Agung Nawawi on 13/10/23.
 //
 
+// An app that uses the SwiftUI app life cycle has a structure that conforms to the App protocol.
+// The structure’s body property returns one or more scenes, which in turn provide content for display.
+// The @main attribute identifies the app’s entry point.
+
 import SwiftUI
 
 @main
